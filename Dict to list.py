0cf6203f56121d7281a968d8500ce26d38d657d0
mgmdvm <mickey@moppin.net>
2019-08-08 15:42:54 -0500

    reorder values in a Dictionary

---
+++ Dict to list.py
@@ -0,0 +1,12 @@
+conditions = {
+"hyperthyroid": 60,
+"a fib": 20,
+"Vit D def": 30
+}
+
+myList = conditions.values()
+print(myList)
+
+#myList.sort(reverse=True) 
+list2 = sorted(myList, reverse=True)
+print(list2)
