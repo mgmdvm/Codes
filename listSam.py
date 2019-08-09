aec79acd0f4dcf113b5abd1a6d50f672d43b5291
mgmdvm <mickey@moppin.net>
2019-08-09 06:50:31 -0500

    Using numpy to work with lists

--- listSam.py
+++ listSam.py
@@ -1,12 +1,25 @@
 import json
 import os
-import string
-import operator
+import numpy as np
 
 
-#Open json file with conditions and percentage ratings
+#Open json file with conditions and ratings
 with open('conditions.json') as disability:
     data = json.load(disability)
-    
-#print(data)
-#print(len(data))
+
+#Create a list of the values from the json dictionary    
+myList = data.values()
+print(myList)
+
+#Sort the list in descending order 
+list2 = sorted(myList, reverse=True)
+print(list2)
+
+#convert list to numpy array
+newList = np.array(list2)
+print(newList)
+
+#convert integers to floats
+newList = newList.astype(np.float32)
+print(newList)
+
